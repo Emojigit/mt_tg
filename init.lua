90ac35ac9b0291ee952f68d1d41117c890ac57bb
1F616EMO <root@1f616emo.xyz>
2024-03-15 06:39:34 +0800

    Ctrl K F

--- init.lua
+++ init.lua
@@ -2,24 +2,24 @@ local http = minetest.request_http_api and minetest.request_http_api()
 if not http then
 	error("Please allow mt_tg to access the HTTP API!")
 end
-local S = minetest.get_translator("mt_tg")
-local storage = minetest.get_mod_storage()
+local S             = minetest.get_translator("mt_tg")
+local storage       = minetest.get_mod_storage()
 
 -- SETTINGS --
-local conf = minetest.settings
+local conf          = minetest.settings
 
 local poll_interval = tonumber(conf:get("mt_tg.poll_interval")) or 5
 local token         = conf:get("mt_tg.token")
 if not token then
 	error("Missing mt_tg.token!")
 end
-local target         = tonumber(conf:get("mt_tg.target")) or error("Missing mt_tg.target!")
-local api_server     = conf:get("mt_tg.api_server")
-if not api_server or api_server  == "" then
+local target     = tonumber(conf:get("mt_tg.target")) or error("Missing mt_tg.target!")
+local api_server = conf:get("mt_tg.api_server")
+if not api_server or api_server == "" then
 	api_server = "https://api.telegram.org/"
 end
 
-local ignored_users  = conf:get("mt_tg.ignored_uesrs") or ""
+local ignored_users   = conf:get("mt_tg.ignored_uesrs") or ""
 local ignored_userids = {}
 for user in string.gmatch(ignored_users, '([^,]+)') do
 	local num = tonumber(user)
@@ -28,18 +28,18 @@ for user in string.gmatch(ignored_users, '([^,]+)') do
 	end
 	ignored_userids[num] = true
 end
-local send_tg_join  = conf:get_bool("mt_tg.send_tg_join",true)
-local send_tg_leave = conf:get_bool("mt_tg.send_tg_leave",true)
-local send_tg_cmds  = conf:get_bool("mt_tg.send_tg_cmds",false)
-local allow_tg_status = conf:get_bool("mt_tg.allow_tg_status",true)
+local send_tg_join    = conf:get_bool("mt_tg.send_tg_join", true)
+local send_tg_leave   = conf:get_bool("mt_tg.send_tg_leave", true)
+local send_tg_cmds    = conf:get_bool("mt_tg.send_tg_cmds", false)
+local allow_tg_status = conf:get_bool("mt_tg.allow_tg_status", true)
 
 -- SETTINGS END --
 
-local my_id = nil -- to be filled by async HTTP
+local my_id           = nil -- to be filled by async HTTP
 
 local function send_tg(msg)
 	local escaped_msg = tostring(msg)
-	escaped_msg = minetest.get_translated_string("en",escaped_msg) or msg
+	escaped_msg = minetest.get_translated_string("en", escaped_msg) or msg
 	escaped_msg = minetest.strip_colors(escaped_msg)
 	http.fetch({
 		url = api_server .. "bot" .. token .. "/sendMessage",
@@ -50,9 +50,9 @@ local function send_tg(msg)
 		},
 		user_agent = "Minetest-Telegram-Relay",
 		multipart = true,
-	},function(resp)
+	}, function(resp)
 		if not resp.succeeded then
-			minetest.log("error","sendMessage Failed, Responce data: " .. resp.data)
+			minetest.log("error", "sendMessage Failed, Responce data: " .. resp.data)
 		end
 	end)
 end
@@ -75,8 +75,9 @@ local function parse_message(msg)
 		local disp_name
 		if message.sender_chat then -- Send on behalf of a chat
 			disp_name = message.sender_chat.title or ("Chn-" .. tostring(message.sender_chat.id))
-		else -- Send by the individual directly
-			disp_name = message.from.first_name .. (message.from.last_name or "") .. (message.from.is_premium and " *" or "")
+		else                  -- Send by the individual directly
+			disp_name = message.from.first_name ..
+			(message.from.last_name or "") .. (message.from.is_premium and " *" or "")
 		end
 		-- DISPLAY NAME END --
 		-- APPEND STR --
@@ -94,10 +95,11 @@ local function parse_message(msg)
 					msg_short = msg
 				end
 			else -- Send by the individual directly
-				rep_disp_name = (message.reply_to_message.from.first_name .. (message.reply_to_message.from.last_name or "")) .. "@TG"
+				rep_disp_name = (message.reply_to_message.from.first_name .. (message.reply_to_message.from.last_name or "")) ..
+				"@TG"
 			end
-			msg_short = string.sub(message.reply_to_message.text or message.reply_to_message.caption or "",1,20)
-			append_str = S("Re @1 \"@2\"",rep_disp_name,msg_short) .. ": "
+			msg_short = string.sub(message.reply_to_message.text or message.reply_to_message.caption or "", 1, 20)
+			append_str = S("Re @1 \"@2\"", rep_disp_name, msg_short) .. ": "
 		else
 			local fwd_name
 			if message.forward_sender_name then -- Private Fwd
@@ -109,64 +111,68 @@ local function parse_message(msg)
 					.. (message.forward_from.is_premium and " *" or "")
 			end
 			if fwd_name then
-				append_str = S("FWD @1",fwd_name) .. ": "
+				append_str = S("FWD @1", fwd_name) .. ": "
 			end
 		end
 		-- APPEND STR END --
 		-- MESSAGE DETECT --
 		if message.new_chat_members then
 			if send_tg_join then
-				for _,y in ipairs(message.new_chat_members) do
+				for _, y in ipairs(message.new_chat_members) do
 					local user_name = y.first_name .. (y.last_name or "")
-					orig_send_all("<" .. S("@1 joined Telegram Group.",user_name) .. ">")
+					orig_send_all("<" .. S("@1 joined Telegram Group.", user_name) .. ">")
 				end
 			end
 			return
 		elseif message.left_chat_member then
 			if send_tg_leave then
 				local user_name = message.left_chat_member.first_name .. (message.left_chat_member.last_name or "")
-				orig_send_all("<" .. S("@1 left Telegram Group.",user_name) .. ">")
+				orig_send_all("<" .. S("@1 left Telegram Group.", user_name) .. ">")
 			end
 			return
 		else
 			-- MESSAGE TYPE DETECT --
 			local text
 			if message.text then -- Plain Text
-				if string.sub(message.text,1,7) == "/status" and allow_tg_status then
+				if string.sub(message.text, 1, 7) == "/status" and allow_tg_status then
 					send_tg(minetest.get_server_status())
 				end
-				if string.sub(message.text,1,1) == "/" and not send_tg_cmds then
+				if string.sub(message.text, 1, 1) == "/" and not send_tg_cmds then
 					return
 				end
 				text = message.text
 			else
 				if message.animation then -- Animations
 					text = "<" .. S("Animation: @1x@2, @3 seconds",
-						message.animation.width,message.animation.height,message.animation.duration) .. ">"
+						message.animation.width, message.animation.height, message.animation.duration) .. ">"
 				elseif message.audio then -- Audio file
 					if message.audio.title then
 						local performer = message.audio.performer or S("Unknown Performer")
-						text = "<" .. S("Audio: @1 by @2, @3 seconds",message.audio.title,performer,message.audio.duration) .. ">"
+						text = "<" ..
+						S("Audio: @1 by @2, @3 seconds", message.audio.title, performer, message.audio.duration) .. ">"
 					else
-						text = "<" .. S("Audio: @1 seconds",message.audio.duration) .. ">"
+						text = "<" .. S("Audio: @1 seconds", message.audio.duration) .. ">"
 					end
 				elseif message.document then -- Do《cuments
 					if message.document.file_name then
-						text = "<" .. S("Document: @1",message.document.file_name) .. ">"
+						text = "<" .. S("Document: @1", message.document.file_name) .. ">"
 					else
 						text = "<" .. S("Document") .. ">"
 					end
 				elseif message.photo then -- Photo
-					text = "<" .. S("Photo: @1x@2",message.photo[#message.photo].width,message.photo[#message.photo].height) .. ">"
+					text = "<" ..
+					S("Photo: @1x@2", message.photo[#message.photo].width, message.photo[#message.photo].height) .. ">"
 				elseif message.sticker then -- Sticker
 					-- text = "<" .. S("Sticker: @1",message.sticker.emoji) .. ">"
 					text = "<" .. S("Sticker") .. ">" -- MT does not support showing emojies!
 				elseif message.video then -- Video
-					text = "<" .. S("Video: @1x@2, @3 seconds",message.video.width,message.video.height,message.video.duration) .. ">"
+					text = "<" ..
+					S("Video: @1x@2, @3 seconds", message.video.width, message.video.height, message.video.duration) ..
+					">"
 				elseif message.videonote then
-					text = "<" .. S("Video Message: @1 seconds",message.videonote.duration) .. ">"
+					text = "<" .. S("Video Message: @1 seconds", message.videonote.duration) .. ">"
 				elseif message.voice then
-					text = "<" .. S("Voice Message: @1 seconds",message.voice.duration) .. ">"
+					text = "<" .. S("Voice Message: @1 seconds", message.voice.duration) .. ">"
 				end
 				if text then
 					if message.caption then
@@ -175,11 +181,11 @@ local function parse_message(msg)
 				else
 					if message.contact then
 						local contact_name = message.contact.first_name .. (message.contact.last_name or "")
-						text = "<" .. S("Contact: @1, @2",contact_name,message.contact.phone_number) .. ">"
+						text = "<" .. S("Contact: @1, @2", contact_name, message.contact.phone_number) .. ">"
 					elseif message.dice then
-						text = "<" .. S("Dice @1: @2",message.dice.emoji,message.dice.value) .. ">"
+						text = "<" .. S("Dice @1: @2", message.dice.emoji, message.dice.value) .. ">"
 					elseif message.game then
-						text = "<" .. S("Game \"@1\": @2",message.game.title,message.game.description) .. ">"
+						text = "<" .. S("Game \"@1\": @2", message.game.title, message.game.description) .. ">"
 					end
 				end
 			end
@@ -197,8 +203,8 @@ local function parse_message(msg)
 	end
 end
 
-local function compareMSGS(a,b)
-  return a.update_id < b.update_id
+local function compareMSGS(a, b)
+	return a.update_id < b.update_id
 end
 
 local function mainloop(first)
@@ -213,53 +219,53 @@ local function mainloop(first)
 			offset = tostring(offset)
 		},
 		user_agent = "Minetest-Telegram-Relay",
-	},function(resp)
+	}, function(resp)
 		if not resp.succeeded then
-			minetest.log("error","getUpdate Failed, Responce data: " .. resp.data)
+			minetest.log("error", "getUpdate Failed, Responce data: " .. resp.data)
 		else
 			local data = minetest.parse_json(resp.data)
 			if not (data and data.result) then
-				minetest.log("error","getUpdate Failed, Responce data: " .. resp.data)
+				minetest.log("error", "getUpdate Failed, Responce data: " .. resp.data)
 			else
-				table.sort(data.result,compareMSGS)
+				table.sort(data.result, compareMSGS)
 				if not first then
-					for _,y in ipairs(data.result) do
-						minetest.log("action","Processing message " .. tostring(y.update_id))
+					for _, y in ipairs(data.result) do
+						minetest.log("action", "Processing message " .. tostring(y.update_id))
 						parse_message(y)
 					end
 				else
 					if data.result[#data.result] then
-						storage:set_int("tg_offset",data.result[#data.result].update_id)
+						storage:set_int("tg_offset", data.result[#data.result].update_id)
 					end
 					minetest.chat_send_all("*** " .. S("Relay set. Messages will be relayed to Telegram group."))
 				end
 			end
 		end
-		minetest.after(first and 1 or poll_interval,mainloop)
+		minetest.after(first and 1 or poll_interval, mainloop)
 	end)
 end
 
 minetest.register_on_mods_loaded(function()
-	minetest.register_on_chat_message(function(name,message)
+	minetest.register_on_chat_message(function(name, message)
 		send_tg(minetest.get_translated_string("en",
 			minetest.format_chat_message(name, message)))
 	end)
 	send_tg("*** Server started!")
-	minetest.after(0,mainloop,true)
+	minetest.after(0, mainloop, true)
 	minetest.after(0, http.fetch, {
 		url = api_server .. "bot" .. token .. "/getMe",
 		method = "GET",
 		user_agent = "Minetest-Telegram-Relay",
 	}, function(resp)
 		if not resp.succeeded then
-			minetest.log("error","getMe Failed, Responce data: " .. resp.data)
+			minetest.log("error", "getMe Failed, Responce data: " .. resp.data)
 		else
 			local data = minetest.parse_json(resp.data)
 			if not (data and data.ok and data.result) then
-				minetest.log("error","getMe Failed, Responce data: " .. resp.data)
+				minetest.log("error", "getMe Failed, Responce data: " .. resp.data)
 			else
 				my_id = data.result.id
-				minetest.log("action","Found bot ID: " .. my_id)
+				minetest.log("action", "Found bot ID: " .. my_id)
 			end
 		end
 	end)
